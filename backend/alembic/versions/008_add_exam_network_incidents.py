463d104e5bd4b7478e1767af5565d6f004e392d1
Abhay Sachan <imabhaysachan@gmail.com>
2026-09-18 18:44:31 +0530

    fix alembic branch head conflict: chain 008_network_incidents after 007_mcq_support

--- backend/alembic/versions/008_add_exam_network_incidents.py
+++ backend/alembic/versions/008_add_exam_network_incidents.py
@@ -1,16 +1,16 @@
 """add_exam_network_incidents
 
-Revision ID: 007_network_incidents
-Revises: 006_target_groups_to_exams
-Create Date: 2026-09-18 00:00:00.000000
+Revision ID: 008_network_incidents
+Revises: 007_mcq_support
+Create Date: 2026-09-18 13:00:00.000000
 
 """
 from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-revision: str = "007_network_incidents"
-down_revision: Union[str, None] = "006_target_groups_to_exams"
+revision: str = "008_network_incidents"
+down_revision: Union[str, None] = "007_mcq_support"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
